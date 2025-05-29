d24e563fde15bb2c42274796e6f66946642ac2bb
sandramarincodes <smarinfernan@gmail.com>
2025-05-29 20:15:08 +0200

    Update collapsible completed

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -1,11 +1,16 @@
 "use strict";
 
-const toggles = document.querySelectorAll('.js_toggle');
+const toggles = document.querySelectorAll(".js_toggle");
 
 for (const toggle of toggles) {
   toggle.addEventListener("click", (ev) => {
     const content = toggle.nextElementSibling;
-    content.classList.toggle('is-open');
-  })
-    
+    content.classList.toggle("is-open");
+
+    if (content.classList.contains("is-open")) {
+      toggle.textContent = toggle.textContent.replace("⬇️", "⬆️");
+    } else {
+      toggle.textContent = toggle.textContent.replace("⬆️", "⬇️");
+    }
+  });
 }
