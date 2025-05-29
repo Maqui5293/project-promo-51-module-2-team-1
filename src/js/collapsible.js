f9a8802b641545a271f16a482a282a340d7f4d78
sandramarincodes <smarinfernan@gmail.com>
2025-05-29 19:54:44 +0200

    Update collapsible effect

--- src/js/collapsible.js
+++ src/js/collapsible.js
@@ -1,2 +1,11 @@
 "use strict";
 
+const toggles = document.querySelectorAll('.js_toggle');
+
+for (const toggle of toggles) {
+  toggle.addEventListener("click", (ev) => {
+    const content = toggle.nextElementSibling;
+    content.classList.toggle('is-open');
+  })
+    
+}
