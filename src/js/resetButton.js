b9cc4871da68c0ece3b9ee509b5b616d59a995d2
maqui5293 <makkss.5293@gmail.com>
2025-06-01 16:06:58 +0200

    update boton

--- src/js/resetButton.js
+++ src/js/resetButton.js
@@ -1,2 +1,24 @@
+"use strict";
 
+// Boton de reset
+const resetBtn = document.querySelector(".js_resetBtn");
 
+// Función para resetear el formulario y la previsualización
+function resetFormAndPreview() {
+  // Resetea los inputs del formulario
+  inputTitle.value = "";
+  inputAutor.value = "";
+  inputGender.value = "";
+  inputRate.value = "";
+  inputUserName.value = "";
+
+  // Limpia la previsualización
+  previewTitle.textContent = "";
+  previewAutor.textContent = "";
+  previewGender.textContent = "";
+  previewRate.textContent = "";
+  previewUserName.textContent = "";
+}
+
+// Evento para el botón
+resetBtn.addEventListener("click", resetFormAndPreview);
