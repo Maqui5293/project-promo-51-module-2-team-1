6f663e33abab32d50a297462a28d138527af3a38
LuciaCarballo <lulucarcas98@gmail.com>
2025-06-05 18:58:41 +0200

    update style

--- src/js/showCard.js
+++ src/js/showCard.js
@@ -1,8 +1,11 @@
+"use strict";
+
 const sectionCard = document.querySelector(".js_card");
 
 if (sectionCard) {
   const params = new URLSearchParams(window.location.search);
   const uuid = params.get("id");
+  
 
   //Petición GET al servidor
 
@@ -22,14 +25,14 @@ if (sectionCard) {
 
 function paintCard(data) {
   sectionCard.innerHTML = `
-    <article class="cardPreview ${data.field7}">
+  <article class="cardPreview ${data.field7}">
       <h2 class="card__title">${data.field2}</h2>               <!-- Título -->
       <p class="card__author">${data.field3}</p>                <!-- Autor -->
       <p class="card__gender">${data.field4}</p>                <!-- Género -->
       <p class="card__stars">${"⭐".repeat(
         data.field5
       )}</p>     <!-- Valoración -->
-      <img class="card__image" src="${
+      <img class="card__image profile__image" src="${
         data.photo
       }" alt="Imagen del libro o recomendación"/> <!-- Imagen -->
       <p class="card__user">Recomendado por: ${data.field6}</p> <!-- Usuario -->
